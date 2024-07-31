d443c80f26d3a9f7e1155515a7d392a42c17da65
imldy <im.ldy@outlook.com>
2024-07-31 20:51:37 +0800

    fix: OPERATION_PLATFORM_U

--- app/constant.ts
+++ app/constant.ts
@@ -13,7 +13,7 @@ export const STABILITY_BASE_URL = "https://api.stability.ai";
 export const DEFAULT_API_HOST = "https://api.nextchat.dev";
 export const OPENAI_BASE_URL = "https://api.openai.com";
 export const ANTHROPIC_BASE_URL = "https://api.anthropic.com";
-export const OPERATION_PLATFORM_URL = "https://aihub.imldy.nl";
+export const OPERATION_PLATFORM_URL = "https://aihub.imldy.fun";
 
 export const GEMINI_BASE_URL = "https://generativelanguage.googleapis.com/";
 
